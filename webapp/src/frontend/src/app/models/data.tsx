f444de8ede581273e84a0c148fda21c1b1f424bd
meysamaghili533@gmail.com <meysamaghili533@gmail.com>
2025-11-09 02:21:11 +0330

    add resume pdf

--- webapp/src/frontend/src/app/models/data.tsx
+++ webapp/src/frontend/src/app/models/data.tsx
@@ -41,7 +41,7 @@ export const AboutData: About = {
   ),
   actions: [
     {
-      href: '/statics/assets/a.pdf',
+      href: '/statics/assets/MeysamAghiliResume.pdf',
       text: 'Resume',
       primary: true,
       Icon: Download,
